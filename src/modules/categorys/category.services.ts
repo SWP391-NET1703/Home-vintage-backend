cdfa46eb35597f9650f77c79a42ef0534c13455e
huyna1707developer@gmail.com <huyna1707developer@gmail.com>
2024-01-29 14:23:24 +0700

    New feature: get list category

--- src/modules/categorys/category.services.ts
+++ src/modules/categorys/category.services.ts
@@ -1,5 +1,5 @@
 import { check } from 'express-validator'
-import { ObjectId } from 'mongodb'
+import { ObjectId, WithId } from 'mongodb'
 import { CreateCategoryReqBody } from './category.request'
 import databaseService from '../database/database.services'
 import Category from './category.schema'
@@ -24,7 +24,42 @@ class CategoryServices {
 
   async getListCategory() {
     const result = await databaseService.categorys.find({}).toArray()
-    return result
+    const categoryChild = result.filter((item) => {
+      if (item.category_parent_id !== '') {
+        return {
+          category_id: item._id,
+          category_parent_id: item.category_parent_id
+        }
+      }
+    })
+
+    const categoryParent = result.filter((item) => {
+      if (item.category_parent_id === '') {
+        return {
+          category_id: item._id,
+          category_parent_id: item.category_parent_id
+        }
+      }
+    })
+
+    const ListCategory: { category_parent: object; category_child: object[] }[] = []
+
+    for (let i = 0; i < categoryParent.length; i++) {
+      const categoryChild = result.filter((item) => {
+        if (item.category_parent_id === String(categoryParent[i]._id)) {
+          return {
+            category_id: item._id,
+            category_parent_id: item.category_parent_id
+          }
+        }
+      })
+
+      ListCategory.push({
+        category_parent: categoryParent[i],
+        category_child: categoryChild
+      })
+    }
+    return ListCategory
   }
 }
 const categoryServices = new CategoryServices()
