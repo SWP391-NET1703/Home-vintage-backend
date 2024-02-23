dcef9dc4948c590960e21c55ba271e6fb27ed706
minatisleeping <minatt2002@gmail.com>
2024-02-23 17:33:01 +0700

    Done verify email user

--- src/modules/users/user.routes.ts
+++ src/modules/users/user.routes.ts
@@ -1,8 +1,20 @@
 import { Router } from 'express'
 
 import { wrapAsync } from '~/utils/handlers'
-import { getProfileController, loginController, logoutController, registerController } from './user.controllers'
-import { accessTokenValidator, loginValidator, refreshTokenValidator, registerValidator } from './user.middlewares'
+import {
+  emailVerifyTokenController,
+  getProfileController,
+  loginController,
+  logoutController,
+  registerController
+} from './user.controllers'
+import {
+  accessTokenValidator,
+  emailVerifyTokenValidator,
+  loginValidator,
+  refreshTokenValidator,
+  registerValidator
+} from './user.middlewares'
 
 const usersRouter = Router()
 
@@ -32,6 +44,19 @@ usersRouter.post('/login', loginValidator, wrapAsync(loginController))
   Body: { refresh_token: string }
 */
 usersRouter.post('/logout', accessTokenValidator, refreshTokenValidator, wrapAsync(logoutController))
+
+/*  
+  Description: Verify email
+  Khi user register,  they will receive an email with a format like this:
+  https://localhost:3000/users/verify-email?token=<email_verify_token>
+  nếu user click vào link này thì sẽ tạo ra req gửi email_verify_token lên server
+  server kiểm tra email_verify_token có hợp lệ không? nếu hợp lệ thì sẽ update email_verified thành true
+  và vào user_id để update email_verified thành '', verify = 1, update_at = new Date()
+  Path: /verify-email
+  Method: POST
+  Body: { email_verify_token: string }
+*/
+usersRouter.post('/verify-email', emailVerifyTokenValidator, wrapAsync(emailVerifyTokenController))
 
 /*
   Des: get profile của user
