965ba286e76a3f945f74ce34b84b4b611de64051
jarvietnguyen318@gmail.com <jarvietnguyen318@gmail.com>
2024-02-26 20:25:27 +0700

    feat : accept, shipping, delete, get list  history order

--- src/modules/orders/order.controllers.ts
+++ src/modules/orders/order.controllers.ts
@@ -7,6 +7,8 @@ import interiorService from '../interiors/interior.services'
 import { INTERIOR_MESSAGES } from '../interiors/interior.messages'
 import { quantityValidator } from './order.middlewares'
 import { OrderDetail } from './order.schema'
+import { OrderStatus } from './order.enum'
+import { TokenPayload } from '../users/User.request'
 
 export const createOrderController = async (req: Request<ParamsDictionary, any, CreateOrderRequest>, res: Response) => {
   const { detail } = req.body as { detail: OrderDetail[] }
@@ -24,5 +26,51 @@ export const createOrderController = async (req: Request<ParamsDictionary, any, 
   res.json({
     message: ORDER_MESSAGES.ORDER_SUCCESSFULL,
     orderInfor: result
+  })
+}
+
+export const acceptOrderController = async (req: Request, res: Response) => {
+  const { id } = req.params
+  const order = await orderService.getOrderById(id)
+  if (order) {
+    const errorMessages = await quantityValidator(order.detail)
+    if (errorMessages.length > 0) {
+      return res.status(422).json({
+        message: errorMessages
+      })
+    }
+  }
+  const result = await orderService.changeStatusOrder(id, OrderStatus.Pack_products)
+  res.json({
+    message: ORDER_MESSAGES.ACCEPT_ORDER_SUCCESSFULL,
+    orderInfor: result
+  })
+}
+
+export const shippingOrderController = async (req: Request, res: Response) => {
+  const { id } = req.params
+  const result = await orderService.changeStatusOrder(id, OrderStatus.Delivery)
+  res.json({
+    message: ORDER_MESSAGES.ORDER_IS_DELIVERIED,
+    orderInfor: result
+  })
+}
+
+export const deleteOrderController = async (req: Request, res: Response) => {
+  const { id } = req.params as { id: string }
+  const result = await orderService.changeStatusOrder(id, OrderStatus.Cancel)
+  res.json({
+    message: ORDER_MESSAGES.ORDER_IS_CANCEL,
+    orderInfor: result
+  })
+}
+
+export const getListOrderHistoryController = async (req: Request, res: Response) => {
+  const { decoded_authorization } = req as { decoded_authorization: TokenPayload }
+  const { user_id } = decoded_authorization
+  const result = await orderService.getListOrderHistory(user_id)
+  res.json({
+    message: ORDER_MESSAGES.GET_LIST_ORDER_HISTORY_SUCCESS,
+    list_order_history: result
   })
 }
--- src/modules/orders/order.middlewares.ts
+++ src/modules/orders/order.middlewares.ts
@@ -1,10 +1,17 @@
-import { checkSchema } from 'express-validator'
+import { check, checkSchema } from 'express-validator'
 import { validate } from '~/utils/validation'
 import { INTERIOR_MESSAGES } from '../interiors/interior.messages'
 import interiorService from '../interiors/interior.services'
-import { OrderDetail } from './order.schema'
+import Order, { OrderDetail } from './order.schema'
 import { ObjectId } from 'mongodb'
 import { ORDER_MESSAGES } from './order.messages'
+import { orderService, updateInteriorQuantity } from './order.services'
+import { OrderStatus } from './order.enum'
+import { TokenPayload } from '../users/User.request'
+import { decode } from 'punycode'
+import { ErrorWithStatus } from '../errors/error.model'
+import HTTP_STATUS from '~/constants/httpStatus'
+import { USERS_MESSAGES } from '~/constants/message'
 
 export const createOrderValidator = validate(
   checkSchema(
@@ -49,6 +56,114 @@ export const quantityValidator = async (detail: OrderDetail[]) => {
       errorMessages.push(errorMessagesLocal.join(', '))
     }
   }
-  console.log(errorMessages)
   return errorMessages
 }
+
+export const acceptOrderValidator = validate(
+  checkSchema(
+    {
+      id: {
+        notEmpty: true,
+        isLength: {
+          options: {
+            max: 24,
+            min: 24
+          },
+          errorMessage: ORDER_MESSAGES.ORDER_IS_NOT_EXIST
+        },
+        custom: {
+          options: async (value, { req }) => {
+            const order = await orderService.getOrderById(value)
+            if (!order) {
+              throw new Error(ORDER_MESSAGES.ORDER_IS_NOT_EXIST)
+            }
+
+            if (
+              [OrderStatus.Pack_products, OrderStatus.Pack_products, OrderStatus.Success].includes(
+                order.status_of_order
+              )
+            ) {
+              throw new Error(ORDER_MESSAGES.ORDER_IS_NOT_VALID_TO_ACCEPT)
+            }
+
+            // quantityValidator(order.detail)
+            updateInteriorQuantity(order.detail)
+            return true
+          }
+        }
+      }
+    },
+    ['params']
+  )
+)
+
+export const shippingOrderValidator = validate(
+  checkSchema(
+    {
+      id: {
+        notEmpty: true,
+        isLength: {
+          options: {
+            min: 24,
+            max: 24
+          },
+          errorMessage: ORDER_MESSAGES.ORDER_IS_NOT_EXIST
+        },
+        custom: {
+          options: async (value, { req }) => {
+            const order = await orderService.getOrderById(value)
+            if (!order) {
+              throw new Error(ORDER_MESSAGES.ORDER_IS_NOT_EXIST)
+            }
+
+            if (order.status_of_order !== OrderStatus.Pack_products) {
+              throw new Error(ORDER_MESSAGES.ORDER_IS_NOT_VALID_TO_DELIVERY)
+            }
+            return true
+          }
+        }
+      }
+    },
+    ['params']
+  )
+)
+
+export const deleteOrderValidator = validate(
+  checkSchema(
+    {
+      id: {
+        notEmpty: true,
+        isLength: {
+          options: {
+            min: 24,
+            max: 24
+          },
+          errorMessage: ORDER_MESSAGES.ORDER_IS_NOT_EXIST
+        },
+        custom: {
+          options: async (value, { req }) => {
+            const order = await orderService.getOrderById(value)
+            if (!order) {
+              throw new Error(ORDER_MESSAGES.ORDER_IS_NOT_EXIST)
+            }
+
+            if (order.status_of_order === OrderStatus.Cancel) {
+              throw new Error(ORDER_MESSAGES.ORDER_IS_NOT_VALID_TO_DELETE)
+            }
+
+            const { decoded_authorization } = req.headers as { decoded_authorization: TokenPayload }
+            const { user_id } = decoded_authorization
+            if (user_id !== order.customer_id.toString()) {
+              throw new ErrorWithStatus({
+                message: USERS_MESSAGES.THIS_IS_NOT_YOUR_ORDER,
+                status: HTTP_STATUS.FORBIDDEN
+              })
+            }
+            return true
+          }
+        }
+      }
+    },
+    ['params']
+  )
+)
