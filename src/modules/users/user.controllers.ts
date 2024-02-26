b9c3f8e1f1875fc6047f2ec71731ad195ed2dfd7
minatisleeping <minatt2002@gmail.com>
2024-02-26 15:43:28 +0700

    Feat: request refresh token

--- src/modules/users/user.controllers.ts
+++ src/modules/users/user.controllers.ts
@@ -6,6 +6,7 @@ import {
   ChangePasswordReqBody,
   LoginReqBody,
   LogoutReqBody,
+  RefreshTokenReqBody,
   RegisterReqBody,
   ResetPasswordReqBody,
   TokenPayload,
@@ -176,4 +177,18 @@ export const changePasswordController = async (
   //gọi hàm changePassword
   const result = await userServices.changePassword(user_id, password)
   return res.json(result)
+}
+
+export const refreshTokenController = async (
+  req: Request<ParamsDictionary, any, RefreshTokenReqBody>,
+  res: Response,
+  next: NextFunction
+) => {
+  const { refresh_token } = req.body
+  const { user_id, verify } = req.decoded_refresh_token as TokenPayload
+  const result = await userServices.refreshToken({ refresh_token, user_id, verify })
+  return res.json({
+    message: USERS_MESSAGES.REFRESH_TOKEN_SUCCESS,
+    result
+  })
 }
